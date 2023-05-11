ce64ecaa9802e406047682a508347c45da72b3d8
carlosgomez247 <carlosgomez.x247@gmail.com>
2023-05-11 14:31:07 -0500

    comentarios

--- chatbot.py
+++ chatbot.py
@@ -4,19 +4,34 @@ from preguntas_respuestas import preguntas_respuestas
 from util import procesar_entrada
 
 def inicializar_modelo(modelo_nombre):
+    # Carga el modelo y el tokenizador
+    # un modelo es una estructura que ha sido entrenada para realizar una tarea específica. En este caso, el modelo es una versión de GPT-Neo, que es una red neuronal de transformadores diseñada para generar texto. El modelo ha sido preentrenado en un gran conjunto de datos y ha aprendido a predecir la próxima palabra en una secuencia de texto.
     model = AutoModelForCausalLM.from_pretrained(modelo_nombre)
+    # Un tokenizador es una herramienta que se utiliza para dividir el texto en "tokens", que son las unidades mínimas de significado que la red neuronal puede entender. Por ejemplo, un token podría ser una palabra, un carácter, o una subpalabra dependiendo del tokenizador. En el caso de GPT-Neo, el tokenizador divide el texto en subpalabras.
     tokenizer = AutoTokenizer.from_pretrained(modelo_nombre)
+    # Determina si hay un dispositivo de GPU disponible y, si no, usa la CPU
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Mueve el modelo al dispositivo correspondiente
     model.to(device)
     return model, tokenizer, device
 
 def generar_respuesta(prompt, model, tokenizer, device):
     entrada_procesada = procesar_entrada(prompt)
+    # Comprueba si la entrada del usuario está en el diccionario de preguntas y respuestas
     if entrada_procesada in preguntas_respuestas:
         return preguntas_respuestas[entrada_procesada]
     else:
         try:
+            # Codifica la entrada del usuario y la pasa al modelo
             input_ids = tokenizer(procesar_entrada(prompt), return_tensors="pt").input_ids.to(device)
+            # Genera una respuesta a partir del modelo
+            # max_length define la longitud máxima de la respuesta
+            # temperature afecta la aleatoriedad de la respuesta. Un valor más alto hace que la respuesta sea más aleatoria
+            # do_sample determina si se debe muestrear aleatoriamente según las probabilidades de las palabras
+            # top_k  Durante el proceso de generación de texto, el modelo seleccionará la próxima palabra de un conjunto de opciones. El parámetro top_k limita estas opciones a las k palabras más probables. Esto puede ayudar a prevenir respuestas poco probables y sin sentido. Sin embargo, un valor demasiado bajo puede hacer que las respuestas sean predecibles y aburridas.
+            # top_p Este es otro método para limitar las opciones de la próxima palabra, conocido como muestreo "nucleus". En lugar de seleccionar de las k palabras más probables, selecciona el conjunto más pequeño de palabras tal que la suma de sus probabilidades es al menos p. Esto puede permitir una mayor diversidad que top_k mientras todavía evita las palabras extremadamente improbables.
+            # num_return_sequences: Este parámetro determina cuántas secuencias de texto independientes se deben generar a partir de cada entrada. En este caso, solo se está generando una secuencia para cada entrada del usuario.
+            # pad_token_id: Este es el ID del token que el tokenizador usa para rellenar secuencias de texto que son más cortas que la longitud máxima permitida. En el caso de GPT-Neo, este es el mismo que el ID del token EOS (End Of Sentence), que indica el final de una secuencia de texto.
             output = model.generate(
                 input_ids=input_ids,
                 max_length=100,
@@ -27,6 +42,7 @@ def generar_respuesta(prompt, model, tokenizer, device):
                 num_return_sequences=1,
                 pad_token_id=tokenizer.eos_token_id
             )
+            # Decodifica la salida del modelo en texto legible
             return tokenizer.decode(output[0], skip_special_tokens=True)
         except Exception as e:
             print(f"Ocurrió un error: {e}")
@@ -34,10 +50,10 @@ def generar_respuesta(prompt, model, tokenizer, device):
 
 def main():
     model, tokenizer, device = inicializar_modelo("./modelo_ajustado")
-    while True:
-        prompt = input("Usuario: ")
+    while True:        
+        prompt = input("Usuario: ")        
         if prompt.lower() == 'salir':
-            break
+            break        
         rta = generar_respuesta(prompt, model, tokenizer, device)
         print(f"Chatbot: {rta}")
 
--- entrenar_modelo.py
+++ entrenar_modelo.py
@@ -5,6 +5,7 @@ MODEL_PATH = "./modelo_ajustado"
 DATA_PATH = './datasets/horario_sof_noche_prosa.txt'
 OUTPUT_PATH = "./resultados_de_entrenamiento"
 
+# Subclase de Trainer con una función de pérdida personalizada
 class MyTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.pop("labels")
@@ -20,29 +21,36 @@ def inicializar_modelo_y_tokenizer(model_path):
 
 def preparar_datasets(tokenizer, data_path, train_size=0.9):
     def tokenize(examples):
+        # Tokenizando y rellenando a la longitud máxima de 128
         return tokenizer(examples["text"], truncation=True, padding='max_length', max_length=128)
     
+    # Cargando y tokenizando el conjunto de datos
     dataset = load_dataset('text', data_files={'train': data_path})
     dataset = dataset['train'].map(tokenize, batched=True)
+    
+    # Dividiendo el conjunto de datos en entrenamiento y validación
     full_dataset = dataset.train_test_split(train_size=train_size)
+    
+    # Preparando los conjuntos de datos finales
     train_dataset = full_dataset['train'].map(lambda examples: {'labels': examples['input_ids']}, batched=True)
     eval_dataset = full_dataset['test'].map(lambda examples: {'labels': examples['input_ids']}, batched=True)
 
     return train_dataset, eval_dataset
 
+# Función para entrenar el modelo
 def entrenar_modelo(model, tokenizer, train_dataset, eval_dataset):
     training_args = TrainingArguments(
-        output_dir=OUTPUT_PATH,
-        overwrite_output_dir=True,
-        num_train_epochs=1,
-        per_device_train_batch_size=1,
-        per_device_eval_batch_size=1,
-        eval_steps=500,
-        save_steps=1000,
-        save_total_limit=2,
-        prediction_loss_only=False,
-        evaluation_strategy="steps",
-        optim="adamw_torch"  
+        output_dir=OUTPUT_PATH,           # Directorio de salida
+        overwrite_output_dir=True,        # Sobreescribir el directorio de salida
+        num_train_epochs=1,               # Número de épocas de entrenamiento
+        per_device_train_batch_size=1,    # Tamaño del lote de entrenamiento por dispositivo
+        per_device_eval_batch_size=1,     # Tamaño del lote de evaluación por dispositivo
+        eval_steps=500,                   # Realizar una evaluación cada 500 pasos
+        save_steps=1000,                  # Guardar el modelo cada 1000 pasos
+        save_total_limit=2,               # Límite para el número total de puntos de control
+        prediction_loss_only=False,       # Si es verdadero, solo la pérdida de predicción se usa para la evaluación
+        evaluation_strategy="steps",      # Estrategia de evaluación
+        optim="adamw_torch"               # Optimizador a utilizar
     )
 
     trainer = MyTrainer(
